2c55124561b2e8a9ba4eb1a358736f8d6ad94754
Samuel Aroney <sam@zooniverse.org>
2018-09-14 11:52:19 +0100

    Add outfile code

--- point_to_lonlat.py
+++ point_to_lonlat.py
@@ -36,7 +36,7 @@ def get_coords_mark(markinfo):
 
 classifications_all = pd.read_csv(infile)
 
-#Make subject dictionary with id as key and metadata
+# Make subject dictionary with id as key and metadata
 subjects_all = pd.read_csv(metafile)
 subjects_dict = {}
 for index, row in subjects_all.iterrows():
@@ -48,25 +48,32 @@ for index, row in subjects_all.iterrows():
 #print(subjects_dict[13308116])
 #print(meta["#scene_corner_UL_x"])
 
-for row in classifications_all:
+#column_names = classifications_all.columns.values.tolist() + ''
+outfile = classifications_all
+# Iterate through classifications, adding lattitude equivalents
+for i, row in enumerate(classifications_all):
     subject_id = row['subject_id']
+    markinfo = subjects_dict[subject_id]
 
-    compiled_data = subjects_dict[subject_id]
-    
-    compiled_data['x_min'] = np.ones_like(marks_subj.subject_id)
-    compiled_data['y_min'] = np.ones_like(marks_subj.subject_id)
-
-    for tool in [0, 1, 2, 3]:
+    for (tool, name) in [(0, 'blockages'), (1, 'floods'), (2, 'shelters'), (3, 'damage')]:
         for df in [0, 1]:
             #data.frame{1,0}.T0_tool{0,1,2,3}_{x,y}
             basename = 'data.frame' + df + '.T0_tool' + tool + '_'
 
-            compiled_data['x'] = eval(row[basename + 'x'])[0]
-            compiled_data['y'] = eval(row[basename + 'y'])[0]
+            markinfo['x'] = eval(row[basename + 'x'])
+            markinfo['y'] = eval(row[basename + 'y'])
+
+            markinfo['x_min'] = 1 #np.ones_like(markinfo['x'])
+            markinfo['y_min'] = 1 #np.ones_like(markinfo['y'])
 
             if tool == 3:
-                compiled_data['details'] = row[basename + 'details']
+                markinfo['details'] = row[basename + 'details']
 
-            if compiled_data['x'] != None & compiled_data['y'] != None:
-                get_coords_mark(compiled_data)
+            if markinfo['x'] != None & markinfo['y'] != None:
+                lon, lat = get_coords_mark(markinfo)
+                outfile[i, name + '_lon'] = lon
+                outfile[i, name + '_lat'] = lat
+
+outfile.to_csv('output_test.csv')
+
 
